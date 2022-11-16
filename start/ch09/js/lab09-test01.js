48a7e2f7d8f07460581c5f205ba4e5b25553a1e3
adindozo <adin.dozo1@outlook.com>
2022-11-16 14:27:22 +0100

    dynamically modifying dom practice

--- start/ch09/js/lab09-test01.js
+++ start/ch09/js/lab09-test01.js
@@ -1,2 +1,13 @@
 /* add code after this comment */
 
+document.getElementById('thumb-list').style.border='1px solid purple';
+
+let ptext= document.querySelector('p').textContent;
+let textArea = document.querySelector('textarea');
+textArea.value=ptext;
+
+let imgs = document.querySelectorAll('img');
+for(let img of imgs) {
+    img.style.boxShadow="0px 0px 20px 4px purple";
+   
+console.log(img)}
--- start/ch09/js/lab09-test02.js
+++ start/ch09/js/lab09-test02.js
@@ -1,3 +1,33 @@
 const photos = JSON.parse(content);
 
 /* put your code after this */
+let sectionContainer = document.getElementById('parent');
+
+for(let photo of photos){
+    let figureNode = document.createElement('figure');
+    let figCNode = document.createElement('figcaption');
+    let h2Node = document.createElement('h2');
+    let h2text = document.createTextNode(photo.title);
+    let pNode = document.createElement('p');
+    let ptext = document.createTextNode(photo.description);
+    let spanNodeArray = [];
+    let imgNode = document.createElement('img');
+    for(let i=0; i<photo.colors.length; i++){
+        spanNodeArray.push(document.createElement('span'));
+        spanNodeArray[i].style.backgroundColor=photo.colors[i].hex;
+    }
+    imgNode.src=`images/${photo.filename}`;
+    imgNode.alt=`images/${photo.filename}`;
+    h2Node.appendChild(h2text);
+    pNode.append(ptext);
+    figCNode.appendChild(h2Node);
+    figCNode.appendChild(pNode);
+    for(let spanNode of spanNodeArray){
+        figCNode.appendChild(spanNode);
+    }
+    figureNode.appendChild(imgNode);
+    figureNode.appendChild(figCNode);
+    sectionContainer.append(figureNode);
+
+
+}
