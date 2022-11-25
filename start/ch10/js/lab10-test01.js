67e3c2f6d1024dcf0a535669dddb41e07a25f54b
adindozo <adin.dozo1@outlook.com>
2022-11-25 19:44:57 +0100

    array additional methods lab

--- start/ch10/js/lab10-test01.js
+++ start/ch10/js/lab10-test01.js
@@ -7,3 +7,64 @@ const stocks = [
  ];
 
  // your solutions here
+for(let stock of stocks){
+  stock.total = function()  {
+    return this.price*this.units;
+  }
+}
+
+stocks.forEach((stock) => {
+  stock.totalForEach= function()  {
+    return this.price*this.units;
+  }
+})
+
+console.log(stocks.find((stock) => {
+  return stock.symbol=='CAT';
+}))
+
+for(let i=0;i<stocks.length;i++){
+  if(stocks[i].symbol=='CAT'){
+    console.log(stocks[i]);
+    break;
+  }
+}
+
+console.log(stocks[0].totalForEach());
+console.log(stocks[0].total());
+
+let newArray = [];
+let strings = [];
+let strings2 = [];
+let newArray2 = stocks.filter((stock) => {
+  return stock.price<20 && stock.price>0;
+});
+
+for(let i=0;i<stocks.length;i++){
+  if(stocks[i].price > 0 && stocks[i].price < 20){
+    newArray.push(stocks[i]);
+    
+  }
+}
+console.log(newArray2);
+console.log(newArray);
+
+for(let i=0;i<stocks.length;i++){
+  strings.push('<li>'+stocks[i].name+'</li>')
+}
+
+strings2 = stocks.map(stock => `<li>${stock.name}</li>`);
+window.onload=function(){
+for(let e of strings){
+  document.querySelector('body').innerHTML+=e;
+}
+}
+console.log(strings);
+console.log(strings2);
+
+console.log(stocks)
+stocks.sort((a,b) => {
+  return a.symbol.localeCompare(b.symbol);
+})
+console.log(stocks)
+
